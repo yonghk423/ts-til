7bd3a6d1cc3b40ea9c57de1016891a53b985d789
yonghk423 <yonghk423423@gmail.com>
2022-04-13 13:28:52 +0900

    class

---
+++ oop-programming/class.ts
@@ -0,0 +1,29 @@
+{
+    type CoffeeCup = {
+        shots: number;
+        hasMilk: boolean;
+    };
+
+    class CoffeeMaker {
+    BEANS_GRAMM_PER_SHOT: number = 7;
+    coffeeBeans: number = 0;
+    
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+        if(this.coffeeBeans < shots * this.BEANS_GRAMM_PER_SHOT) {            
+        throw new Error('NOT enough coffe beans!')
+        }
+        this.coffeeBeans -= shots * this.BEANS_GRAMM_PER_SHOT;
+        return {
+        shots,
+        hasMilk: false,
+        }    
+        }
+    }
+
+    const maker = new CoffeeMaker(32);
+    console.log(maker);
+}
